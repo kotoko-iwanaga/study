bda0145413a4f51eb877498f7e6b949e9b63acdc
kotoko-iwanaga <bettynoirev@gmail.com>
2023-11-15 01:40:41 +0300

    Сделана 3 лаба

--- structure-labs/Lab-3/Lab-3-1/Lab-3-1.cpp
+++ structure-labs/Lab-3/Lab-3-1/Lab-3-1.cpp
@@ -10,55 +10,121 @@
 #include "CharWork.h"
 
 
-typedef struct Node {
+typedef struct List {
     char c;
-    struct Node* next;
-} Node;
+    struct List* next;
+} List;
 
 
-void push(Node** head, char data) {
-    Node* tmp = (Node*)malloc(sizeof(Node));
+void push(List** head, char data) {
+    List* tmp = (List*)malloc(sizeof(List));
     tmp->c = data;
     tmp->next = (*head);
     (*head) = tmp;
 }
-void printList(const Node* head) {
+void printList(const List* head) {
     while (head) {
         printf("%c", head->c);
         head = head->next;
     }
 }
 
-int main() {
-    char z = 0;
-    Node* head = NULL;
-    int size = 100;
-    char* str = (char*)calloc(size,sizeof(char));
-    fgets(str, size, stdin);
-    for (int i = strlen(str)-1; i >= 0; i--) {
-        push(&head, str[i]);
+void getFromArray(List** head, char* str) {
+    for (int i = strlen(str) - 2; i >= 0; i--) {
+        push(head, str[i]);
     }
-    printList(head);
-    
-    
+}
+
+void setMinWord(int& temp, int& min, int& pos, int& temp_pos) {
+    if (temp != 0) {
+        pos = min >= temp ? temp_pos : pos;
+        min = min >= temp ? temp : min;
+    }
+    temp = 0;
 }
 
 
-//char* str;
-//str = get_string_by_malloc(stdin); // сам массив символов
-//int str_size = strlen(str); // размер массива
-//int min_word = str_size + 1; // длина мин слова
-//int min_word_pos = 0; // последний символ наикороткого слова
-//// поиск длины мин слова
-//findMinWord(str, strlen(str), min_word, min_word_pos);
-//// обработка строки
-//moveChar(str, strlen(str), min_word, min_word_pos);
-//// вывод результата и некоторой статистики
-//printf("%s\n", str);
-//free(str); // освобождаем память
+void getMinLength(const List* head, int& min, int& pos) {
+    int temp_min_word = 0;
+    int i = 0;
+    while (head) { // находим длину наименьшего слова
+        switch (head->c) {
+        case '\n':
+        case '\0':
+        case ' ':
+            setMinWord(temp_min_word, min, pos, i); 
+            break;
+        default:
+            temp_min_word++;
+        }
+        head = head->next;
+        i++;
+    }
+    setMinWord(temp_min_word, min, pos, i);
+}
+
+char* getMinWord(const List* head, int pos, int min_length) {
+    char* minimum = (char*) malloc(sizeof(char)*min_length);
+    for (int i = 0; head; i++) {
+        if (i >= pos-min_length && i < pos) {
+            minimum[i - (pos - min_length)] = head->c;
+        }
+        else if (i >= pos) {
+            break;
+        }
+        head = head->next;
+
+    }
+    minimum[min_length] = '\0';
+    return minimum;
+}
 
 
+void insert(List* head, unsigned n, int val) {
+    unsigned i = 0;
+    List* tmp = NULL;
+    //Находим нужный элемент. Если вышли за пределы списка, то выходим из цикла,
+    //ошибка выбрасываться не будет, произойдёт вставка в конец
+    while (i < n && head->next) {
+        head = head->next;
+        i++;
+    }
+    tmp = (List*)malloc(sizeof(List));
+    tmp->c = val;
+    //Если это не последний элемент, то next перекидываем на следующий узел
+    if (head->next) {
+        tmp->next = head->next;
+        //иначе на NULL
+    }
+    else {
+        tmp->next = NULL;
+    }
+    head->next = tmp;
+}
 
-//printf("Minimum length: %d\n", min_word);
-//printf("End position: %d\n", min_word_pos - 1);
-//printf("Length of string: %d\n", (int)strlen(str));
+void doubleMinWord(List* head, char* minimum, int min_length, int pos) {
+    for (int i = 0; i < min_length; i++) {
+        insert(head, pos - 1 + i, minimum[i]);
+    }
+}
+
+int main() {
+    List* head = NULL;
+    int position = 0;
+    int min_length = 707*707;
+    int size = 100;
+    char* str = (char*)calloc(size,sizeof(char));
+
+    fgets(str, size, stdin);
+    getFromArray(&head, str);
+    getMinLength(head, min_length, position);
+    char* min_word = getMinWord(head, position, min_length);
+    printf("min_l: %d\n min_w: %s \n position: %d\n", min_length, min_word, position);
+
+    doubleMinWord(head, min_word, min_length, position);
+    
+    printList(head); 
+    
+
+    
+}
