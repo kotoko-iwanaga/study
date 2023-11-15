077da8561366e1107f443d5e7c8bf61c8b80af19
kotoko-iwanaga <bettynoirev@gmail.com>
2023-11-15 22:43:42 +0300

    Вынес функции списка в отдельный файл заголовка

--- structure-labs/Lab-3/Lab-3-1/Lab-3-1.cpp
+++ structure-labs/Lab-3/Lab-3-1/Lab-3-1.cpp
@@ -7,26 +7,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "List.h"
+
+void getFromArray(List**, char*);
+void setMinWord(int&, int&, int&, int&);
+void getMinLength(const List*, int&, int&);
+char* getMinWord(const List*, int, int);
+void doubleMinWord(List*, char*, int, int);
 
 
-typedef struct List {
-    char c;
-    struct List* next;
-} List;
+int main() {
+    List* head = NULL;
+    int position = 0;
+    int min_length = 707*707;
+    int size = 100;
+    char* str = (char*)calloc(size,sizeof(char));
 
+    fgets(str, size, stdin);
+    getFromArray(&head, str);
+    getMinLength(head, min_length, position);
+    char* min_word = getMinWord(head, position, min_length);
+    printf("min_l: %d\nmin_w: %s\nposition: %d\n", min_length, min_word, position);
 
-void push(List** head, char data) {
-    List* tmp = (List*)malloc(sizeof(List));
-    tmp->c = data;
-    tmp->next = (*head);
-    (*head) = tmp;
+    doubleMinWord(head, min_word, min_length, position);
+    printList(head); 
+
+    
 }
-void printList(const List* head) {
-    while (head) {
-        printf("%c", head->c);
-        head = head->next;
-    }
-}
+
 
 void getFromArray(List** head, char* str) {
     for (int i = strlen(str) - 2; i >= 0; i--) {
@@ -51,7 +59,7 @@ void getMinLength(const List* head, int& min, int& pos) {
         case '\n':
         case '\0':
         case ' ':
-            setMinWord(temp_min_word, min, pos, i); 
+            setMinWord(temp_min_word, min, pos, i);
             break;
         default:
             temp_min_word++;
@@ -63,9 +71,9 @@ void getMinLength(const List* head, int& min, int& pos) {
 }
 
 char* getMinWord(const List* head, int pos, int min_length) {
-    char* minimum = (char*) malloc(sizeof(char)*min_length);
+    char* minimum = (char*)malloc(sizeof(char) * min_length);
     for (int i = 0; head; i++) {
-        if (i >= pos-min_length && i < pos) {
+        if (i >= pos - min_length && i < pos) {
             minimum[i - (pos - min_length)] = head->c;
         }
         else if (i >= pos) {
@@ -78,50 +86,8 @@ char* getMinWord(const List* head, int pos, int min_length) {
     return minimum;
 }
 
-
-void insert(List* head, unsigned n, int val) {
-    unsigned i = 0;
-    List* tmp = NULL;
-    //Находим нужный элемент. Если вышли за пределы списка, то выходим из цикла,
-    //ошибка выбрасываться не будет, произойдёт вставка в конец
-    while (i < n && head->next) {
-        head = head->next;
-        i++;
-    }
-    tmp = (List*)malloc(sizeof(List));
-    tmp->c = val;
-    //Если это не последний элемент, то next перекидываем на следующий узел
-    if (head->next) {
-        tmp->next = head->next;
-        //иначе на NULL
-    }
-    else {
-        tmp->next = NULL;
-    }
-    head->next = tmp;
-}
-
 void doubleMinWord(List* head, char* minimum, int min_length, int pos) {
     for (int i = 0; i < min_length; i++) {
         insert(head, pos - 1 + i, minimum[i]);
     }
-}
-
-int main() {
-    List* head = NULL;
-    int position = 0;
-    int min_length = 707*707;
-    int size = 100;
-    char* str = (char*)calloc(size,sizeof(char));
-
-    fgets(str, size, stdin);
-    getFromArray(&head, str);
-    getMinLength(head, min_length, position);
-    char* min_word = getMinWord(head, position, min_length);
-    printf("min_l: %d\n min_w: %s \n position: %d\n", min_length, min_word, position);
-
-    doubleMinWord(head, min_word, min_length, position);
-    
-    printList(head); 
-    
 }
---
+++ structure-labs/Lab-3/Lab-3-1/List.h
@@ -0,0 +1,91 @@
+#pragma once
+typedef struct List {
+    char c;
+    struct List* next;
+} List;
+
+void push(List**, char);
+int pop(List**);
+void printList(const List*);
+void insert(List*, unsigned, int);
+List* getNth(List*, int);
+int deleteNth(List**, int);
+
+
+
+
+
+void push(List** head, char data) {
+    List* tmp = (List*)malloc(sizeof(List));
+    tmp->c = data;
+    tmp->next = (*head);
+    (*head) = tmp;
+}
+
+int pop(List** head) { // удаление первого элемента
+    List* prev = NULL;
+    int val;
+    if (head == NULL) {
+        exit(-1);
+    }
+    prev = (*head);
+    val = prev->c;
+    (*head) = (*head)->next;
+    free(prev);
+    return val;
+}
+
+void printList(const List* head) {
+    while (head) {
+        printf("%c", head->c);
+        head = head->next;
+    }
+}
+
+void insert(List* head, unsigned n, int val) {
+    unsigned i = 0;
+    List* tmp = NULL;
+    //Находим нужный элемент. Если вышли за пределы списка, то выходим из цикла,
+    //ошибка выбрасываться не будет, произойдёт вставка в конец
+    while (i < n && head->next) {
+        head = head->next;
+        i++;
+    }
+    tmp = (List*)malloc(sizeof(List));
+    tmp->c = val;
+    //Если это не последний элемент, то next перекидываем на следующий узел
+    if (head->next) {
+        tmp->next = head->next;
+        //иначе на NULL
+    }
+    else {
+        tmp->next = NULL;
+    }
+    head->next = tmp;
+}
+
+List* getNth(List* head, int n) {
+    int counter = 0;
+    while (counter < n && head) {
+        head = head->next;
+        counter++;
+    }
+    return head;
+}
+
+int deleteNth(List** head, int n) {
+    if (n == 0) {
+        return pop(head);
+    }
+    else {
+        List* prev = getNth(*head, n - 1);
+        List* elm = prev->next;
+        int val = elm->c;
+
+        prev->next = elm->next;
+        free(elm);
+        return val;
+    }
+}
+
+
