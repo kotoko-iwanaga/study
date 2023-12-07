38a5cd604908508ecf21fb98dc423550a08cbf90
Melty <mmminami@yandex.ru>
2023-12-07 23:26:41 +0300

    Первая лаба готова

--- high-level-programming/Lab-1/Lab-1.cpp
+++ high-level-programming/Lab-1/Lab-1.cpp
@@ -8,13 +8,22 @@
 #include <iostream>
 #include <vector>
 #include <locale>
+
 using namespace std;
 
 class Array {
     vector<int> arr;
 public:
-    Array(vector<int> tmp) {
-        arr = tmp;
+    Array() {}
+    Array(int size) {
+        arr.resize(size);
+    }
+    void setArray() {
+        for (int i = 0; i < arr.capacity(); i++)
+            cin >> arr[i];
+    }
+    vector<int> getArray() {
+        return arr;
     }
     bool checkByDecrease() {
         for (int i = 0; i < arr.capacity()-1; i++) {
@@ -27,51 +36,100 @@ public:
         return true;
     }
     void printArr() {
-        cout << "Вывод массива из класса Array размерностью: " << arr.capacity() << endl;
         for (int i = 0; i < arr.capacity(); i++) {
             cout << arr[i] << " ";
         }
         cout << endl;
     }
-    vector<int> getArray() {
-        return arr;
-    }
 };
 
 class Matrix : Array {
-    int str;
-    Array matrix[];
-public:
-    Matrix() { 
+    vector<Array> matrix;
+
+    bool isPrime(int number) {
+        for (int i = 2; i <= number / 2; ++i)
+            if (number % i == 0)
+                return false;
+        return true;
     }
-    void print() {
-        printf("%d", Array::checkByDecrease());
+    bool checkRowIncrease(int row) {
+        for (int j = 0; j < matrix[row].getArray().capacity() - 1; j++) {
+            if (matrix[row].getArray()[j] >= matrix[row].getArray()[j + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool checkColumnIncrease(int column) {
+        for (int i = 0; i < matrix.capacity() - 1; i++) {
+            if (matrix[i].getArray()[column] >= matrix[i + 1].getArray()[column]) {
+                return false;
+            }
+        }
+        return true;
+    }
+public:
+    Matrix(vector<Array> tmp) {
+        matrix = tmp;
+    }
+    bool isSimple(int row) {
+        for (int i = 0; i < matrix[row].getArray().capacity(); i++) {
+            if (isPrime(matrix[row].getArray()[i]) == false) {
+                return false;
+            }
+        }
+        return true;
+    }
+    int columnsByIncrease() {
+        int result = 0;
+        for (int j = 0; j < matrix[0].getArray().capacity(); j++) {
+            if (checkColumnIncrease(j) == true) {
+                result++;
+            }
+        }
+        return result;
+    }
+    int rowsByIncrease() {
+        int result = 0;
+        for (int i = 0; i < matrix.capacity(); i++) {
+            if (checkRowIncrease(i) == true) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    void printMatrix() {
+        for (int i = 0; i < matrix.capacity(); i++)
+            matrix[i].printArr();
     }
 };
 
-void print(vector<int> A, int size) {
-    cout << "Вывод массива из мейна размерностью " << size << endl;
-    for (int i = 0; i < size; i++)
-        cout << A[i] << " ";
-    cout << endl;
+bool isPrime(int number) {
+    for (int i = 2; i <= number / 2; ++i)
+        if (number % i == 0)
+            return false;
+    return true;
 }
 
 int main()
 {
     setlocale(0, "");
-    int array_size = 0;
+    int rows = 0, columns = 0; // строки и столбцы;
 
-    cout << "Введите размер массива: ";
-    cin >> array_size;
+    cout << "Введите размеры матрицы: ";
+    cin >> rows >> columns;
+    cout << "Введите элементы массива" << endl;
+    vector<Array> origArray(rows, Array(columns));
 
-    vector<int> origArray(array_size);
-    for (auto i = 0; i < array_size; i++)
-        cin >> origArray[i];
-    
+    for (int i = 0; i < rows; i++)
+        origArray[i].setArray();
 
-    Array arr(origArray);
-    arr.printArr();
-    cout << arr.checkByDecrease();
+    Matrix matrix(origArray);
+    cout << "Вот ваша матрица" << endl;
+    matrix.printMatrix();
+    cout << "Сколько строк возрастает по порядку: " << matrix.rowsByIncrease() << endl;
+    cout << "Сколько столбцов возрастает по порядку: " << matrix.columnsByIncrease() << endl;
+    cout << "Первая строка с простыми числами? " << matrix.isSimple(1) << endl;
 
-    return 0;
 }
