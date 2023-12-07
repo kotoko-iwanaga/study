ae25509d7ef3e9b2bd95ea68d9becc11235b9b1c
Melty <mmminami@yandex.ru>
2023-12-07 14:33:18 +0300

    ага

--- high-level-programming/Lab-1/Lab-1.cpp
+++ high-level-programming/Lab-1/Lab-1.cpp
@@ -1,71 +1,77 @@
-﻿// Lab-1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿/*Создать класс, определяющий одномерный массив и содержащий функцию, проверяющую, 
+упорядочен ли этот одномерный массив по убыванию. 
+Для заданной матрицы определить, сколько ее строк упорядочены по возрастанию. 
+Создать класс-наследник, позволяющий определить, состоит ли массив только из простых чисел. 
+Сколько столбцов данной матрицы A(i,j упорядочены по возрастанию?
+*/
 
 #include <iostream>
+#include <vector>
 #include <locale>
 using namespace std;
 
 class Array {
-    int size;
-    int* newArray = 0;
+    vector<int> arr;
 public:
-    Array(int sizeArr, int *origArray) {
-        size = sizeArr;
-        newArray = new int(size);
-        for (int i = 0; i < size; i++)
-            newArray[i] = origArray[i];
+    Array(vector<int> tmp) {
+        arr = tmp;
     }
     bool checkByDecrease() {
-        for (int i = 0; i < (size-1); i++) {
-            if (newArray[i] < newArray[i + 1]) { 
+        for (int i = 0; i < arr.capacity()-1; i++) {
+            if (arr[i] < arr[i + 1]) {
+                cout << "Массив не упорядочен по убыванию" << endl;
                 return false;
             }
         }
+        cout << "Массив упорядочен по убыванию" << endl;
         return true;
     }
-    void printArray() {
-        for (int i = 0; i < size; i++)
-            cout << newArray[i];
+    void printArr() {
+        cout << "Вывод массива из класса Array размерностью: " << arr.capacity() << endl;
+        for (int i = 0; i < arr.capacity(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
     }
-    int *getArray() {
-        return newArray;
+    vector<int> getArray() {
+        return arr;
     }
 };
 
-int* inputToArray(int size) {
-    int* array = new int(size);
-    for (int i = 0; i < size; i++) {
-        cin >> array[i];
+class Matrix : Array {
+    int str;
+    Array matrix[];
+public:
+    Matrix() { 
     }
-    return array;
+    void print() {
+        printf("%d", Array::checkByDecrease());
+    }
+};
+
+void print(vector<int> A, int size) {
+    cout << "Вывод массива из мейна размерностью " << size << endl;
+    for (int i = 0; i < size; i++)
+        cout << A[i] << " ";
+    cout << endl;
 }
 
 int main()
 {
     setlocale(0, "");
-    int sizeArr = 0;
+    int array_size = 0;
 
     cout << "Введите размер массива: ";
-    cin >> sizeArr;
+    cin >> array_size;
 
-    int* origArray = new int(sizeArr);
-    origArray = inputToArray(sizeArr);
+    vector<int> origArray(array_size);
+    for (auto i = 0; i < array_size; i++)
+        cin >> origArray[i];
+    
 
-    Array arr{ sizeArr, origArray[5]};
-    arr.printArray();
-    int* test = 0;
-    memcpy(test, arr.getArray(), sizeArr);
-    //cout << arr.checkByDecrease();
+    Array arr(origArray);
+    arr.printArr();
+    cout << arr.checkByDecrease();
 
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
